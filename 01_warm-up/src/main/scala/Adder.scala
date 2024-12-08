997ea29dc624a8edf0053d0e403a0efb96f523d3
shangeeth <shangeeth2k@gmail.com>
2024-12-08 22:34:00 +0100

    Added codes for adders and Readserial

--- 01_warm-up/src/main/scala/Adder.scala
+++ 01_warm-up/src/main/scala/Adder.scala
@@ -16,19 +16,24 @@ import chisel3.util._
   * Your task is to implement a basic half adder as presented in the lecture.
   * Each signal should only be one bit wide (inputs and outputs).
   * There should be no delay between input and output signals, we want to have
-  * a combinational behaviour of the component.
+  * a combinational behaviour of the component.s
   */
 class HalfAdder extends Module{
   
   val io = IO(new Bundle {
-    /* 
-     * TODO: Define IO ports of a half adder as presented in the lecture
-     */
-    })
-
+      /*
+       * TODO: Define IO ports of a half adder as presented in the lecture
+       */
+       val a = Input (UInt(1.W))
+       val b = Input (UInt(1.W))
+       val s = Output (UInt(1.W))
+       val c = Output (UInt(1.W))
+      })
   /* 
    * TODO: Describe output behaviour based on the input values
    */
+       io.s := io.a ^ io.b // sum is the XOR of inputs
+       io.c := io.a & io.b // Carry is the AND of inputs
 
 }
 
@@ -49,18 +54,39 @@ class FullAdder extends Module{
     /* 
      * TODO: Define IO ports of a half adder as presented in the lecture
      */
-    })
+     val a = Input (UInt(1.W))
+     val b = Input (UInt(1.W))
+     val c = Input (UInt(1.W))
+     val sum = Output (UInt(1.W))
+     val carry = Output (UInt(1.W))
+     })
+     val sum1 = Wire(UInt(1.W))
+     val carry1 = Wire(UInt(1.W))
+     val carry2 = Wire(UInt(1.W))
 
 
   /* 
    * TODO: Instanciate the two half adders you want to use based on your HalfAdder class
    */
+     val halfadder1 = Module(new HalfAdder())
+     val halfadder2 = Module(new HalfAdder())
+     //Connections half adder1
+     halfadder1.io.a := io.a
+     halfadder1.io.b := io.b
+     sum1 := halfadder1.io.s
+     carry1 := halfadder1.io.c
+
+     //Connections half adder2
+     halfadder2.io.a := sum1
+     halfadder2.io.b := io.c
+     carry2 := halfadder2.io.c
 
 
   /* 
    * TODO: Describe output behaviour based on the input values and the internal signals
    */
-
+     io.sum := halfadder2.io.s
+     io.carry := carry1| carry2
 }
 
 /** 
@@ -79,14 +105,47 @@ class FourBitAdder extends Module{
     /* 
      * TODO: Define IO ports of a 4-bit ripple-carry-adder as presented in the lecture
      */
+    val a, b = Input(UInt(4.W))
+    val sum, carry = Output(UInt(4.W))
     })
+    val tempsum = Wire(UInt(4.W))
+    val tempcarry = Wire(UInt(1.W))
+
 
   /* 
    * TODO: Instanciate the full adders and one half adderbased on the previously defined classes
    */
+    val halfadder = Module(new HalfAdder())
+    val fulladder1 = Module(new FullAdder())
+    val fulladder2 = Module(new FullAdder())
+    val fulladder3 = Module(new FullAdder())
+  // Half Adder for the LSB
+    halfadder.io.b := io.b(0)
+    tempsum(0) := halfadder.io.s
+    tempcarry := halfadder.io.c
+  // Full Adder for the second bit
+    fulladder1.io.a := io.a(1)
+    fulladder1.io.b := io.b(1)
+    fulladder1.io.c := tempcarry
+    tempsum(1) := fulladder1.io.sum
+    tempcarry := fulladder1.io.carry
+  // Full Adder for the third bit
+    fulladder2.io.a := io.a(2)
+    fulladder2.io.b := io.b(2)
+    fulladder2.io.c := tempcarry
+    tempsum(2) := fulladder2.io.sum
+    tempcarry := fulladder2.io.carry
+  // Full Adder for the MSB
+    fulladder3.io.a := io.a(3)
+    fulladder3.io.b := io.b(3)
+    fulladder3.io.c := tempcarry
+    tempsum(3) := fulladder3.io.sum
+    tempcarry := fulladder3.io.carry
 
 
   /* 
    * TODO: Describe output behaviour based on the input values and the internal 
    */
+     io.carry  := tempcarry
+     io.sum  := tempsum
 }
--- 01_warm-up/src/main/scala/ReadSerial.scala
+++ 01_warm-up/src/main/scala/ReadSerial.scala
@@ -10,47 +10,82 @@ import chisel3._
 import chisel3.util._
 
 
-/** controller class */
-class Controller extends Module{
-  
+/** Controller class */
+class Controller extends Module {
+
   val io = IO(new Bundle {
-    /* 
-     * TODO: Define IO ports of a the component as stated in the documentation
-     */
-    })
+    val rst = Input(UInt(1.W))         // reset signal
+    val serial_bus = Input(UInt(1.W))  // bus input
+    val end = Input(UInt(1.W))         // end of 8bits from counter
+    val ready = Output(UInt(1.W))      // valid signal for output ready
+    val start = Output(UInt(1.W))      // counter trigger
+  })
 
-  // internal variables
-  /* 
-   * TODO: Define internal variables (registers and/or wires), if needed
-   */
+  // Internal registers for the state machine and control signals
+  val out_ready = RegInit(0.U(1.W))  // Register for output ready
+  val count_start = RegInit(0.U(1.W)) // Register to trigger the counter
 
-  // state machine
-  /* 
-   * TODO: Describe functionality if the controller as a state machine
-   */
-
+  // State machine functionality
+  when(io.rst === 1.U) {
+    // Reset condition
+    io.ready := 0.U
+    io.start := 0.U
+    out_ready := 0.U
+  } .elsewhen(io.serial_bus === 0.U && out_ready === 1.U) {
+    // Condition where serial_bus is 0 and out_ready is set
+    io.start := 1.U
+    out_ready := 0.U
+  } .elsewhen(io.end === 1.U) {
+    // End of 8 bits
+    out_ready := 1.U
+    io.ready := 1.U
+  } .otherwise {
+    // Default case
+    io.start := 0.U
+    io.ready := 0.U
+    out_ready := 0.U //  to avoid unintended active state
+  }
 }
-
 
 /** counter class */
 class Counter extends Module{
   
   val io = IO(new Bundle {
+
+    val rst = Input(UInt(1.W))
+    val start = Input(UInt(1.W))
+    val end = Output(UInt(1.W))
     /* 
      * TODO: Define IO ports of a the component as stated in the documentation
      */
     })
-
   // internal variables
   /* 
    * TODO: Define internal variables (registers and/or wires), if needed
    */
+  val counter = RegInit(0.U(3.W))
 
   // state machine
   /* 
    * TODO: Describe functionality if the counter as a state machine
    */
-
+  when(io.rst === 1.U)
+  {
+    // reset state
+    counter = 0.U
+    io.end := 0.U
+  }.elsewhen(counter == 7.U )
+  {
+    //if counter = 8 , output is ready
+    io.end := 1.U
+    counter :=0.U
+  }.elsewhen(io.start === 1.U){
+    // start of the counter
+    counter := counter + 1.U
+  }.otherwise{
+    //default state
+    io.end := 0.U
+  }
 
 }
 
@@ -61,8 +96,10 @@ class ShiftRegister extends Module{
     /* 
      * TODO: Define IO ports of a the component as stated in the documentation
      */
+    val serial_bus = Input(UInt(1.W))
+    val parallel_out = Output(UInt(8.W))
     })
-
+    val internal_reg = RegInit(0.U(8.W))
   // internal variables
   /* 
    * TODO: Define internal variables (registers and/or wires), if needed
@@ -72,6 +109,9 @@ class ShiftRegister extends Module{
   /* 
    * TODO: Describe functionality if the shift register
    */
+  internal_reg:= Cat(io.serial_bus, internal_reg(7, 1)) // Shift right and insert new serial bit at MSB
+  parllel_out := internal_reg
+
 }
 
 /** 
@@ -91,22 +131,38 @@ class ReadSerial extends Module{
     /* 
      * TODO: Define IO ports of a the component as stated in the documentation
      */
+    val reset_n = Input(UInt(1.W))
+    val rxd = Input(UInt(1.W))
+    val valid = Output(UInt(1.W))
+    val data = Output(UInt(8.W))
     })
-
-
+  val counter_end = Wire(UInt(1.W))
+  val counter_start = Wire(UInt(1.W))
   // instanciation of modules
   /* 
    * TODO: Instanciate the modules that you need
    */
-
+  val Controller = Module (new Controller())
+  val Counter = Module(new Counter())
+  val ShiftRegister = Module( new ShiftRegister())
   // connections between modules
   /* 
    * TODO: connect the signals between the modules
    */
+  //controller connections
+  Controller.io.rst := io.reset_n
+  Controller.io.serial := io.rxd
+  Controller.io.end := counter_end
+  io.valid := Controller.io.ready
+  counter_start := Controller.io.start
 
-  // global I/O 
-  /* 
-   * TODO: Describe output behaviour based on the input values and the internal signals
-   */
+  // Counter connections
+  Counter.io.rst := io.reset_n
+  Counter.io.start:= counter_start
+  counter_end := Counter.io.end
+
+  // Shift register connections
+  ShiftRegister.io.serial_bus := io.rxd
+  io.data := ShiftRegister.io.parallel_out
 
 }
