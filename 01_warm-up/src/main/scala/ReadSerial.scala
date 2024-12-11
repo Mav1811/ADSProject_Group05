b70af5bf5e2777d1d1a312a1a77bf3dc4794c6f9
shangeeth <shangeeth2k@gmail.com>
2024-12-11 19:50:37 +0100

    modified codes for adders and Readserial

--- 01_warm-up/src/main/scala/ReadSerial.scala
+++ 01_warm-up/src/main/scala/ReadSerial.scala
@@ -1,168 +1,179 @@
-// ADS I Class Project
-// Chisel Introduction
-//
-// Chair of Electronic Design Automation, RPTU in Kaiserslautern
-// File created on 18/10/2022 by Tobias Jauch (@tojauch)
-
 package readserial
 
 import chisel3._
 import chisel3.util._
+import chisel3.experimental.ChiselEnum
 
+object ControllerState extends ChiselEnum {
+  val START_TRANSFER, WAIT, RESET = Value
+}
 
-/** Controller class */
-class Controller extends Module {
+object CounterState extends ChiselEnum {
+  val START_COUNT, COUNT, RESET = Value
+}
 
+class ControllerModule extends Module {
   val io = IO(new Bundle {
     val rst = Input(UInt(1.W))         // reset signal
     val serial_bus = Input(UInt(1.W))  // bus input
-    val end = Input(UInt(1.W))         // end of 8bits from counter
+    val count_s = Input(UInt(3.W))     // Counter value
     val ready = Output(UInt(1.W))      // valid signal for output ready
     val start = Output(UInt(1.W))      // counter trigger
   })
 
-  // Internal registers for the state machine and control signals
+  // Import the states from ControllerState
+  import ControllerState._
+
+  // Internal register for the FSM state
+  val state = RegInit(START_TRANSFER) // Initial state is START_TRANSFER
   val out_ready = RegInit(0.U(1.W))  // Register for output ready
   val count_start = RegInit(0.U(1.W)) // Register to trigger the counter
 
-  // State machine functionality
-  when(io.rst === 1.U) {
-    // Reset condition
-    io.ready := 0.U
-    io.start := 0.U
-    out_ready := 0.U
-  } .elsewhen(io.serial_bus === 0.U && out_ready === 1.U) {
-    // Condition where serial_bus is 0 and out_ready is set
-    io.start := 1.U
-    out_ready := 0.U
-  } .elsewhen(io.end === 1.U) {
-    // End of 8 bits
-    out_ready := 1.U
-    io.ready := 1.U
-  } .otherwise {
-    // Default case
-    io.start := 0.U
-    io.ready := 0.U
-    out_ready := 0.U //  to avoid unintended active state
+  // Default assignments
+  io.start := 0.U
+  io.ready := 0.U
+
+  // State machine logic using switch/case
+  switch(state) {
+    is(START_TRANSFER) {
+      // Start the transfer by starting the counter
+      when(io.serial_bus === 0.U && io.count_s === 0.U) {
+        io.start := 1.U // Trigger the counter to start
+        state := WAIT    // Transition to the WAIT state
+      }.elsewhen(io.rst === 1.U){
+        state := RESET
+      }
+    }
+    is(WAIT) {
+      io.start := 0.U
+      // Wait until counter reaches 8
+      when(io.count_s === 8.U) {
+        io.ready := 1.U  // Set ready when 8 bits are received
+        state := START_TRANSFER // Transition to START_TRANSFER state to begin a new transfer
+      }.elsewhen(io.rst === 1.U) {
+        state := RESET  // If reset signal is high, transition to RESET state
+      }.otherwise {
+        state := WAIT    // Stay in WAIT state
+      }
+    }
+    is(RESET) {
+      // Reset state
+      io.ready := 0.U // Reset ready signal
+      state := START_TRANSFER // Transition back to START_TRANSFER state to initiate the next transfer
+    }
   }
 }
 
-/** counter class */
-class Counter extends Module{
-  
+class Counter extends Module {
   val io = IO(new Bundle {
+    val rst = Input(UInt(1.W))     // reset signal
+    val start = Input(UInt(1.W))   // start signal to trigger counting
+    val count_s = Output(UInt(3.W)) // 3-bit counter output
+  })
 
-    val rst = Input(UInt(1.W))
-    val start = Input(UInt(1.W))
-    val end = Output(UInt(1.W))
-    /* 
-     * TODO: Define IO ports of a the component as stated in the documentation
-     */
-    })
-  // internal variables
-  /* 
-   * TODO: Define internal variables (registers and/or wires), if needed
-   */
-  val counter = RegInit(0.U(3.W))
+  import CounterState._
 
-  // state machine
-  /* 
-   * TODO: Describe functionality if the counter as a state machine
-   */
-  when(io.rst === 1.U)
-  {
-    // reset state
-    counter = 0.U
-    io.end := 0.U
-  }.elsewhen(counter == 7.U )
-  {
-    //if counter = 8 , output is ready
-    io.end := 1.U
-    counter :=0.U
-  }.elsewhen(io.start === 1.U){
-    // start of the counter
-    counter := counter + 1.U
-  }.otherwise{
-    //default state
-    io.end := 0.U
+  // Internal registers
+  val state = RegInit(START_COUNT)  // Initial state is START_COUNT
+  val counter = RegInit(0.U(3.W))  // 3-bit counter, initialized to 0
+
+  io.count_s := counter  // Output the counter value
+
+  // State machine logic
+  switch(state) {
+    is(START_COUNT) {
+      // Wait for the 'start' signal to begin counting
+      when(io.start === 1.U) {
+        state := COUNT  // Transition to COUNT state when 'start' signal is received
+      }.elsewhen(io.rst === 1.U) {
+        state := RESET  // Reset state if reset signal is high
+      }
+    }
+
+    is(COUNT) {
+      // Counting logic
+      when(counter === 7.U) {
+        counter := 0.U  // Reset counter after reaching 7
+        state := START_COUNT // Change the state
+      }.otherwise {
+        counter := counter + 1.U  // Increment counter
+      }
+      // Reset condition
+      when(io.rst === 1.U) {
+        state := RESET  // Transition to RESET state if reset signal is high
+      }
+    }
+
+    is(RESET) {
+      // Reset the counter and go back to START_COUNT state
+      counter := 0.U
+      when(io.rst === 0.U) {
+        state := START_COUNT  // Go back to START_COUNT when reset is released (0)
+      }
+    }
   }
-
 }
 
-/** shift register class */
 class ShiftRegister extends Module{
-  
   val io = IO(new Bundle {
-    /* 
-     * TODO: Define IO ports of a the component as stated in the documentation
-     */
     val serial_bus = Input(UInt(1.W))
     val parallel_out = Output(UInt(8.W))
-    })
-    val internal_reg = RegInit(0.U(8.W))
-  // internal variables
-  /* 
-   * TODO: Define internal variables (registers and/or wires), if needed
-   */
+  })
 
-  // functionality
-  /* 
-   * TODO: Describe functionality if the shift register
-   */
-  internal_reg:= Cat(io.serial_bus, internal_reg(7, 1)) // Shift right and insert new serial bit at MSB
-  parllel_out := internal_reg
+  val internal_reg = RegInit(0.U(8.W))
+  io.parallel_out := 0.U
 
+  // Functionality
+  internal_reg := Cat(io.serial_bus, internal_reg(7, 1)) // Shift right and insert new serial bit at MSB
+  io.parallel_out := internal_reg
 }
 
-/** 
-  * The last warm-up task deals with a more complex component. Your goal is to design a serial receiver.
-  * It scans an input line (“serial bus”) named rxd for serial transmissions of data bytes. A transmission 
-  * begins with a start bit ‘0’ followed by 8 data bits. The most significant bit (MSB) is transmitted first. 
-  * There is no parity bit and no stop bit. After the last data bit has been transferred a new transmission 
-  * (beginning with a start bit, ‘0’) may immediately follow. If there is no new transmission the bus line 
-  * goes high (‘1’, this is considered the “idle” bus signal). In this case the receiver waits until the next 
-  * transmission begins. The outputs of the design are an 8-bit parallel data signal and a valid signal. 
-  * The valid signal goes high (‘1’) for one clock cycle after the last serial bit has been transmitted, 
-  * indicating that a new data byte is ready.
-  */
-class ReadSerial extends Module{
-  
+class ReadSerial extends Module {
   val io = IO(new Bundle {
-    /* 
-     * TODO: Define IO ports of a the component as stated in the documentation
-     */
     val reset_n = Input(UInt(1.W))
     val rxd = Input(UInt(1.W))
     val valid = Output(UInt(1.W))
     val data = Output(UInt(8.W))
-    })
-  val counter_end = Wire(UInt(1.W))
+  })
+
+  // Declare wires
+  val counter_value = Wire(UInt(3.W))
   val counter_start = Wire(UInt(1.W))
-  // instanciation of modules
-  /* 
-   * TODO: Instanciate the modules that you need
-   */
-  val Controller = Module (new Controller())
-  val Counter = Module(new Counter())
-  val ShiftRegister = Module( new ShiftRegister())
-  // connections between modules
-  /* 
-   * TODO: connect the signals between the modules
-   */
-  //controller connections
-  Controller.io.rst := io.reset_n
-  Controller.io.serial := io.rxd
-  Controller.io.end := counter_end
-  io.valid := Controller.io.ready
-  counter_start := Controller.io.start
+
+  // Registers to avoid combinational loop
+  val counter_reg = RegInit(0.U(3.W)) // Register for counter_end
+  val counter_start_reg = RegInit(0.U(1.W)) // Register for counter_start
+
+  // Instantiate modules
+  val controller = Module(new ControllerModule())
+  val counter = Module(new Counter())
+  val shiftRegister = Module(new ShiftRegister())
+
+  io.valid := 0.U
+  io.data := 0.U
+
+  // Update the registers with the wire values after each clock cycle
+  counter_reg := counter_value
+  counter_start_reg := counter_start
+
+  // Controller connections
+  controller.io.rst := io.reset_n
+  controller.io.serial_bus := io.rxd
+  controller.io.count_s := counter_value
+  io.valid := controller.io.ready
+  counter_start := controller.io.start
 
   // Counter connections
-  Counter.io.rst := io.reset_n
-  Counter.io.start:= counter_start
-  counter_end := Counter.io.end
+  counter.io.rst := io.reset_n
+  counter.io.start := counter_start_reg
+  counter_value := counter.io.count_s
 
   // Shift register connections
-  ShiftRegister.io.serial_bus := io.rxd
-  io.data := ShiftRegister.io.parallel_out
+  shiftRegister.io.serial_bus := io.rxd
+  io.data := shiftRegister.io.parallel_out
 
+  when(io.reset_n === 1.U) {
+    counter_reg := 0.U
+    counter_start_reg := 0.U
+  }
 }
--- 01_warm-up/src/test/scala/ReadSerialTester.scala
+++ 01_warm-up/src/test/scala/ReadSerialTester.scala
@@ -11,22 +11,83 @@ import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
 
-/** 
-  *read serial tester
-  */
+/**
+ *read serial tester
+ */
 class ReadSerialTester extends AnyFlatSpec with ChiselScalatestTester {
 
   "ReadSerial" should "work" in {
     test(new ReadSerial).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      // Reset the device under test (DUT)
+      dut.io.reset_n.poke(0.U) // Assert reset
+      dut.clock.step(1)         // Allow time for reset
+      dut.io.reset_n.poke(1.U) // Deassert reset
+      dut.clock.step(1) // Allow some time to come out of reset
 
-        /*dut.io.rxd.poke(...)
-         *dut.clock.step(...)
-         *dut.io.valid.expect(...)
-         *dut.io.data.expect("b11111111".U) 
-         *...
-         *TODO: Add your testcases here
-         */
-        }
-    } 
+      // test case 1
+      dut.io.reset_n.poke(0.U)
+      dut.io.rxd.poke(0.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U) // transmission started and 8 bits transmitted
+      dut.clock.step(1)
+      dut.io.rxd.poke(0.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(1)
+      // output check
+      dut.io.valid.expect(1.B)
+      dut.io.data.expect("b10111111".U)
+      // continous transmission without idle time
+      dut.io.rxd.poke(0.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U) // transmission started and 8 bits transmitted
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(1)
+      // output check
+      dut.io.valid.expect(1.B)
+      dut.io.data.expect("b11111111".U)
+      // continous high signal no transmission
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(10)
+      dut.io.valid.expect(0.B)
+      dut.io.data.expect(0.U)
+      // incomplete transmission
+      dut.io.rxd.poke(0.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(0.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      dut.clock.step(1)
+      dut.io.rxd.poke(1.U)
+      // output check
+      dut.io.valid.expect(0.B)
+      dut.io.data.expect(0.U)
+      //
+    }
+  }
 }
-
