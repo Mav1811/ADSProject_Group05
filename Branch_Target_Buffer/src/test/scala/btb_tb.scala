cc230e5c2ac1028eedcc63eb2d5fc4df38087fab
shangeeth <shangeeth2k@gmail.com>
2025-03-27 18:11:39 +0100

    changes after meeting in testcases

--- Branch_Target_Buffer/src/test/scala/btb_tb.scala
+++ Branch_Target_Buffer/src/test/scala/btb_tb.scala
@@ -12,8 +12,13 @@ class BTBTest extends AnyFlatSpec with ChiselScalatestTester {
       // Initialize inputs to the BTB
       dut.clock.setTimeout(0)  // Remove timeout
 
+      var PC_1 = "hFBCD0000"
+      var PC_2 = "hABCF0000"
+      var PC_3 = "hABFF0000"
+
+
       // Test Case 1: Write to the BTB and check valid entry
-      dut.io.PC.poke("hABCD0000".U)
+      dut.io.PC.poke(PC_1.U)
       dut.io.update.poke(0.U)  // No update initially
       dut.io.updatePC.poke(0.U)
     //  dut.io.updateTarget.poke(0.U)
@@ -25,7 +30,7 @@ class BTBTest extends AnyFlatSpec with ChiselScalatestTester {
 
       // Write the first instruction
       dut.io.update.poke(1.U)  // Now allow updates to the BTB
-      dut.io.updatePC.poke("hABCD0000".U)  // Writing for PC "hABCD0000"
+      dut.io.updatePC.poke(PC_1.U)  // Writing for PC "hABCD0000"
       dut.io.updateTarget.poke(10.U)  // Target address 10
       dut.io.mispredicted.poke(1.U)  // Simulate a misprediction
 
@@ -33,55 +38,59 @@ class BTBTest extends AnyFlatSpec with ChiselScalatestTester {
 
       // Checking if the entry is valid and the target is correctly updated
       dut.io.update.poke(0.U)
-      dut.io.PC.poke("hABCD0000".U)
+      dut.io.PC.poke(PC_1.U)
       dut.clock.step(1)
       dut.io.valid.expect(1.U)  // BTB entry should now be valid
       dut.io.target.expect(10.U)  // Target should be 10
       dut.io.predictedTaken.expect(1.U)  // initial value is 01 - mispredicted - current state will be 10-
 
-      //----------- Test Case 2: Reading and writing at the same PC address
-      dut.io.PC.poke("hABCF0000".U)
+
+
+      // Reading and writing at the same PC address - forwarding logic in BTB
+      dut.io.PC.poke(PC_2.U)
       dut.io.update.poke(1.U)  // Writing to the BTB at the same PC
-      dut.io.updatePC.poke("hABCF0000".U)
+      dut.io.updatePC.poke(PC_2.U)
       dut.io.updateTarget.poke(30.U)
       dut.io.mispredicted.poke(1.U)
       dut.clock.step(1)
 
-      // Ensure correct valid and target
       dut.io.valid.expect(1.U)
       dut.io.target.expect(30.U)
       dut.io.predictedTaken.expect(1.U)
-
+      dut.io.update.poke(0.U)
+      //_________ LRU_________//
+   // Now we are reading the pc_1 and lru is toogled and now points to pc_2
       dut.clock.step(1)
-      dut.io.PC.poke("hABCD0000".U)
+      dut.io.PC.poke(PC_1.U)    // toggle the LRU bit
       dut.clock.step(1)
-      dut.io.PC.poke("hABCF0000".U)
       dut.io.valid.expect(1.U)
-      dut.io.target.expect(30.U)
-      dut.io.predictedTaken.expect(0.U)
+      dut.io.target.expect(10.U)
+      dut.io.predictedTaken.expect(1.U)
       dut.clock.step(1)
 
-      // Test Case 3: Checking Replacement Logic in BTB
-      dut.io.update.poke(1.U)  // Write a new entry at the same set
-      dut.io.updatePC.poke("hABFF0000".U)
+      // we are now updating pc_3 so it will replace pc_2
+
+      dut.io.update.poke(1.U)
+      dut.io.updatePC.poke(PC_3.U)
       dut.io.updateTarget.poke(40.U)
       dut.io.mispredicted.poke(0.U)
 
       dut.clock.step(1)
       dut.io.update.poke(0.U)
-      dut.io.PC.poke("hABCD0000".U)  // This entry should be evicted
+      dut.io.PC.poke(PC_2.U)  // This entry should be evicted
       dut.clock.step(1)
-      dut.io.valid.expect(0.U)  // Entry for "hABCD0000" should no longer be valid
+      dut.io.valid.expect(0.U)  // Entry for pc_2 should no longer be valid
 
       dut.clock.step(1)
-      dut.io.PC.poke("hABCF0000".U)
+      dut.io.PC.poke(PC_1.U)
       dut.clock.step(1)
-      dut.io.valid.expect(1.U)
-      dut.io.target.expect(30.U)
-      dut.io.predictedTaken.expect(0.U)
+      dut.io.valid.expect(1.U)     // should be getting valid for pc_1 and pc_3
+      dut.io.target.expect(10.U)
+      dut.io.predictedTaken.expect(1.U)
 
       dut.clock.step(1)
-      dut.io.PC.poke("hABFF0000".U)
+      dut.io.update.poke(0.U)
+      dut.io.PC.poke(PC_3.U)
       dut.clock.step(1)
       dut.io.valid.expect(1.U)
       dut.io.target.expect(40.U)
@@ -89,19 +98,21 @@ class BTBTest extends AnyFlatSpec with ChiselScalatestTester {
 
       // Step 4: FSM Test - State Transitions
 
-      // Test FSM transition from strongNotTaken → weakNotTaken → strongTaken
-      dut.io.PC.poke("hABCD0000".U)
+      // Test FSM transition from strongNotTaken → weakNotTaken → strongTaken same pc value is used
+
+      dut.io.PC.poke("hABCD0000".U)    // now starts at this pc
       dut.io.update.poke(1.U)
       dut.io.updatePC.poke("hABCD0000".U)
       dut.io.updateTarget.poke("hCDEF0000".U)
-      dut.io.mispredicted.poke(0.U)
+      dut.io.mispredicted.poke(0.U)        // intially pc is mispredicted is false so will be start in state strongNOtTaken
+
       dut.clock.step(1)
       dut.io.valid.expect(1.U)
       dut.io.target.expect("hCDEF0000".U)
-      dut.io.predictedTaken.expect(0.U)  // strongNotTaken initially- 00
+      dut.io.predictedTaken.expect(0.U)  // verified pc state strongNotTaken initially- 00
 
       // Step 1: First misprediction should move to weakNotTaken
-      dut.io.mispredicted.poke(1.U)
+      dut.io.mispredicted.poke(1.U)     // now changes to weak not taken
       dut.clock.step(1)
       dut.io.update.poke(0.U)
       dut.io.predictedTaken.expect(0.U)  // weakNotTaken - 01
@@ -109,23 +120,24 @@ class BTBTest extends AnyFlatSpec with ChiselScalatestTester {
 
       // Step 2: Second misprediction should move to strongTaken
       dut.io.update.poke(1.U)
-      dut.io.mispredicted.poke(1.U)
+      dut.io.mispredicted.poke(1.U)     // now changes to  StrongTaken
       dut.clock.step(1)
       dut.io.update.poke(0.U)
       dut.io.predictedTaken.expect(1.U)  // current state -10- strong taken
 
-      // Step 3: Verify final state after all transitions
+      // Step 3 : Third misprediction should move to weakTaken
       dut.io.PC.poke("hABCD0000".U)
       dut.io.update.poke(1.U)
       dut.io.updatePC.poke("hABCD0000".U)
       dut.io.updateTarget.poke("hCDEF0000".U)
       dut.io.mispredicted.poke(1.U)
       dut.clock.step(1)
+
       dut.io.update.poke(0.U)
       dut.io.predictedTaken.expect(1.U)  // Verify the FSM is in weakTaken state- current state 11
 
 
-      //step 4: Verify the state transitions
+      //step 4: Verify the state transitions  all mispredicted so transition is like 01(initial value) -> 10-> 11 -> 00 -> 01
       dut.io.updatePC.poke(155.U)
       dut.io.updateTarget.poke("hCDEF0000".U)
       dut.io.update.poke(1.U)
@@ -146,7 +158,7 @@ class BTBTest extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.PC.poke(155.U)
       dut.io.update.poke(0.U)
       dut.clock.step(1)
-      dut.io.predictedTaken.expect(1.U)
+      dut.io.predictedTaken.expect(1.U)  // 11
       ///
       dut.io.updatePC.poke(155.U)
       dut.io.updateTarget.poke("hCDEF0000".U)
@@ -157,7 +169,7 @@ class BTBTest extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.PC.poke(155.U)
       dut.io.update.poke(0.U)
       dut.clock.step(1)
-      dut.io.predictedTaken.expect(0.U)
+      dut.io.predictedTaken.expect(0.U)  // 00
       ////
       dut.io.updatePC.poke(155.U)
       dut.io.updateTarget.poke("hCDEF0000".U)
@@ -168,7 +180,7 @@ class BTBTest extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.PC.poke(155.U)
       dut.io.update.poke(0.U)
       dut.clock.step(1)
-      dut.io.predictedTaken.expect(0.U)
+      dut.io.predictedTaken.expect(0.U)  //01
 
       ///step5: BTB indexing check
 
